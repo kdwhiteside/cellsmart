b9be7a5162493d4cbf72919f2ef7abe827321063
Kevin Whiteside 1 <kevindwhiteside@icloud.com>
2016-12-06 16:14:04 -0500

    testing

--- server.js
+++ server.js
@@ -2,17 +2,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-//Require Schemas
-export MONGOLAB_URI="mongodb://example:example@ds053312.mongolab.com:53312/todolist"
-var Article = require('./server/model.js');
+var path = require('path');
 
 // Create Instance of Express
 var app = express();
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
-
-// Run Morgan for Logging
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
@@ -22,16 +16,22 @@ app.use(express.static('./public'));
 
 // -------------------------------------------------
 
-// MongoDB Configuration configuration
-mongoose.connect('mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk');
-var db = mongoose.connection;
-db.on('error', function (err) {
-	console.log('Mongoose Error: ', err);
-});
+// // MongoDB Configuration configuration
+// mongoose.connect('mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk');
+// var db = mongoose.connection;
+// db.on('error', function (err) {
+// 	console.log('Mongoose Error: ', err);
+// });
 
-db.once('open', function () {
-	console.log('Mongoose connection successful.');
-});
+// db.once('open', function () {
+// 	console.log('Mongoose connection successful.');
+// });
+
+
+//Require Schemas
+// var Article = require('./server/model.js');
+// var MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
 
 // -------------------------------------------------
 
@@ -40,54 +40,22 @@ app.get('/', function(req, res){
 	res.sendFile('./public/index.html');
 })
 
-// Route to get all saved articles
-app.get('/api/saved', function(req, res) {
-
-	Article.find({})
-		.exec(function(err, doc){
-
-			if(err){
-				console.log(err);
-			}
-			else {
-				res.send(doc);
-			}
-		})
+app.get('/repairs', function (req, res) {
+	res.sendFile(path.join(__dirname, './public/repairs.html'));
 });
 
-// Route to add an article to saved list
-app.post('/api/saved', function(req, res){
-	var newArticle = new Article(req.body);
-
-	console.log(req.body)
-
-	var title = req.body.title;
-	var date = req.body.date;
-	var url = req.body.url;
-
-	newArticle.save(function(err, doc){
-		if(err){
-			console.log(err);
-		} else {
-			res.send(doc._id);
-		}
-	});
+app.get('/iphone-repairs', function (req, res) {
+	res.sendFile(path.join(__dirname, './public/iphone-repairs.html'));
 });
 
-// Route to delete an article from saved list
-app.delete('/api/saved/', function(req, res){
+app.get('/google-repairs'), function(req, res) {
+	res.sendFile('./public/google-repairs.html')
+}
 
-	var url = req.param('url');
+app.get('/tablet-repairs'), function(req, res) {
+	res.sendFile('./public/tablet-repairs.html')
+}
 
-	Article.find({"url": url}).remove().exec(function(err, data){
-		if(err){
-			console.log(err);
-		}
-		else {
-			res.send("Deleted");
-		}
-	});
-});
 
 // -------------------------------------------------
 
